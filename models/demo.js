35b9156df4d63bf4e6c64679ff3a36e3fe26925e
Gergő Radeczki <g.radeczki@gmail.com>
2021-01-08 18:07:46 +0100

    demo scheme added

---
+++ models/demo.js
@@ -0,0 +1,12 @@
+const Schema = require('mongoose').Schema;
+const db = require('../config/db');
+
+const Demo = db.model('Jegyzet', {
+    _foreignKey: {
+        type: Schema.Types.ObjectId,
+        ref: 'ReferencedScheme'
+    },
+    name: String,
+});
+
+module.exports = Jegyzet;
